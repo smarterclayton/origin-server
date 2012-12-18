a11280895e7dc72841c10ebf61514d05fadda4f7
Rajat Chopra <rchopra@redhat.com>
2012-12-18 14:10:45 -0800

    admin-ctl-app remove particular gear

--- plugins/msg-broker/mcollective/lib/openshift/mcollective_application_container_proxy.rb
+++ plugins/msg-broker/mcollective/lib/openshift/mcollective_application_container_proxy.rb
@@ -668,6 +668,12 @@ module OpenShift
         reply
       end
 
+      def move_gear_secure(app, gear, destination_container, destination_district_uuid, allow_change_district, node_profile)
+        Application.run_in_application_lock(app) do
+          move_gear(app, gear, destination_container, destination_district_uuid, allow_change_district, node_profile)
+        end
+      end
+
       def move_gear(app, gear, destination_container, destination_district_uuid, allow_change_district, node_profile)
         reply = ResultIO.new
         state_map = {}
